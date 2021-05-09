0d40fe9e34eb43c4b554c42243206cb44e7dfa3d
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-05-09 10:28:00 +0530

    app - use serverSideProps

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Main({ githubData, quote }) {
     );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
     const response = await fetch(
         "https://api.github.com/users/ingeniousambivert/repos?per_page=100&sort=created:desc&client_id=22ea12584240e7a2631f&client_secret=bc2deb37cb23d714f3b16483ba93a4a0e61f93e9",
         {
