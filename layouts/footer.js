201a58edaeb94da73fac7d8d3411b87f6b3e2de9
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2023-05-09 19:35:37 +0530

    update footer design

--- layouts/footer.js
+++ layouts/footer.js
@@ -1,22 +1,22 @@
 export default function Footer({ quote }) {
-    return (
-        <footer className=" bg-black text-white rounded-b-lg">
-            <div className="container mx-auto p-10">
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                    <div>
-                        <p className="text-3xl md:text-4xl">Inspiration </p>
-                        <p className="mt-4 italic">{quote}</p>
-                    </div>
-                    <div className="text-right md:mt-12">
-                        <a
-                            href="mailto:monarchmaisuriya7600@gmail.com"
-                            className="hover:text-gray-300">
-                            monarchmaisuriya7600@gmail.com
-                        </a>
-                        <p>Made with &#9829;</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className=" bg-black text-white rounded-b-lg">
+      <div className="container mx-auto p-10">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div>
+            <p className="italic">{quote}</p>
+          </div>
+          <div className="text-center md:text-right">
+            <a
+              href="mailto:monarchmaisuriya7600@gmail.com"
+              className="hover:text-gray-300"
+            >
+              monarchmaisuriya7600@gmail.com
+            </a>
+            <p>Made with &#9829;</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
