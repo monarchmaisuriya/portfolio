ab17b806dc8bea4bddd65f866294b33c4062ca77
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2024-01-28 23:12:55 +0530

    revamp

--- layouts/header.js
+++ layouts/header.js
@@ -17,15 +17,6 @@ export default function Header() {
 								<RiGithubLine />
 							</a>
 						</li>
-
-						<li className=" pr-5 text-xl md:text-2xl hover:text-gray-400 text-white transition duration-300 mt-0.5 ">
-							<a
-								href="https://www.upwork.com/o/profiles/users/~01434db45df923780a/"
-								target="_blank"
-							>
-								<SiUpwork />
-							</a>
-						</li>
 						<li className=" pr-5 text-xl md:text-2xl hover:text-gray-400 text-white transition duration-300">
 							<a
 								href="https://www.linkedin.com/in/monarchmaisuriya/"
@@ -34,11 +25,20 @@ export default function Header() {
 								<RiLinkedinFill />
 							</a>
 						</li>
-						<li className=" pr-5 text-xl md:text-2xl hover:text-gray-400 text-white transition duration-300">
+						<li className=" pr-5 text-xl md:text-2xl hover:text-gray-400 text-white transition duration-300 mt-0.5 ">
+							<a
+								href="https://www.upwork.com/o/profiles/users/~01434db45df923780a/"
+								target="_blank"
+							>
+								<SiUpwork />
+							</a>
+						</li>
+
+						{/* <li className=" pr-5 text-xl md:text-2xl hover:text-gray-400 text-white transition duration-300">
 							<a href="https://ingeniousambivert.medium.com/" target="_blank">
 								<SiMedium />
 							</a>
-						</li>
+						</li> */}
 					</ul>
 				</div>
 			</div>
