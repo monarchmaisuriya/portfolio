50b1c7ff3b04544beef852bd420e3ede38205591
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-03-08 09:12:31 +0530

    fix spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import Header from "./layouts/header";
-import Footer from "./layouts/footer";
-import About from "./containers/about";
-import Skills from "./containers/skills";
-import Work from "./containers/work";
-import Contact from "./containers/contact";
-import { randomQuote } from "./utils";
+import Header from "./layouts/header"
+import Footer from "./layouts/footer"
+import About from "./containers/about"
+import Skills from "./containers/skills"
+import Work from "./containers/work"
+import Contact from "./containers/contact"
+import { randomQuote } from "./utils"
 
 export default function App() {
-	const quote = randomQuote();
+	const quote = randomQuote()
 	return (
 		<div>
 			<div className="flex justify-center flex-col min-h-screen font-modern">
 				<main className="flex-grow">
 					<div className="mx-auto my-4 md:my-10 px-4 md:px-12">
-						<div className="container flex justify-center px-4 my-9">
+						<div className="flex justify-center px-4 my-9">
 							<a href="https://about.me/monarchmaisuriya" target="_blank">
 								<img src="favicon-dark.png" alt="logo" width={20} height={20} />
 							</a>
@@ -29,8 +29,7 @@ export default function App() {
 							<a
 								className="hover:text-gray-800"
 								href="https://about.me/monarchmaisuriya"
-								target="_blank"
-							>
+								target="_blank">
 								Monarch Maisuriya
 							</a>
 						</div>
@@ -38,5 +37,5 @@ export default function App() {
 				</main>
 			</div>
 		</div>
-	);
+	)
 }
